4b01bbce7b14a72e72a5245c8938e262ab0f701c
NiklasToetsch <niklas.toetsch@bayer.com>
2025-05-21 12:15:55 +0200

    enable 5x5 cross-validation

--- ml.py
+++ ml.py
@@ -14,32 +14,31 @@ def create_pipeline():
 ])
 
 
-def run_cv(X, y, groups=None, cv_spec=StratifiedGroupKFold(n_splits=5), training_name=""):
+def run_cv(X, y, groups=None, n_splits=5, n_repetitions=5, training_name=""):
     splits = []
 
-    if isinstance(cv_spec, StratifiedGroupKFold):
+    for i in tqdm.tqdm(range(n_repetitions), total=n_repetitions, desc=f"Repetitions"):
+        cv_spec = StratifiedGroupKFold(n_splits=n_splits)
         split_obj = cv_spec.split(X, y, groups=groups)
-    else:
-        split_obj = cv_spec.split(X, y)
+    
+        for ii, indeces in tqdm.tqdm(enumerate(split_obj), total=n_splits, desc=f"Splits"):
+            train_index, val_index = indeces
+            X_train_fold, X_val_fold = X.iloc[train_index], X.iloc[val_index]
+            y_train_fold, y_val_fold = y.iloc[train_index], y.iloc[val_index]
+            
+            # Fit the pipeline
+            pipeline = create_pipeline()
+            PIPELINE_REGISTRY[f"{training_name}_{i}.{ii}"] = pipeline
+            pipeline.fit(X_train_fold, y_train_fold)
+            
+            # Make predictions
+            y_pred = pipeline.predict(X_val_fold)
+            y_hat_proba = pipeline.predict_proba(X_val_fold)
 
-    for i, indeces in tqdm.tqdm(enumerate(split_obj), total=5):
-        train_index, val_index = indeces
-        X_train_fold, X_val_fold = X.iloc[train_index], X.iloc[val_index]
-        y_train_fold, y_val_fold = y.iloc[train_index], y.iloc[val_index]
-        
-        # Fit the pipeline
-        pipeline = create_pipeline()
-        PIPELINE_REGISTRY[f"{training_name}_{i}"] = pipeline
-        pipeline.fit(X_train_fold, y_train_fold)
-        
-        # Make predictions
-        y_pred = pipeline.predict(X_val_fold)
-        y_hat_proba = pipeline.predict_proba(X_val_fold)
-
-        splits.append(
-            {"y": y_val_fold, 
-            "y_hat": y_pred, 
-            "y_hat_proba": y_hat_proba, 
-            "val_index": val_index})
+            splits.append(
+                {"y": y_val_fold, 
+                "y_hat": y_pred, 
+                "y_hat_proba": y_hat_proba, 
+                "val_index": val_index})
 
     return splits
